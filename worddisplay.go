22aa390d2b25df11e3b9df2c8852896b5f64866f
coquentin <quentin.contreau@ynov.com>
2024-01-02 20:23:49 +0100

    ho ho ho

--- worddisplay.go
+++ worddisplay.go
@@ -1,13 +1,34 @@
 package hangman
 
-import "fmt"
+import (
+	"math/rand"
+	"strings"
+)
 
-func DisplayWord(revealed []bool, word string) {
-	for i, c := range word {
-		if revealed[i] {
-			fmt.Printf("%c ", c)
-		} else {
-			fmt.Print("_ ")
+//	func DisplayWord(revealed []bool, word string) {
+//		for i, c := range word {
+//			if revealed[i] {
+//				fmt.Printf("%c ", c)
+//			} else {
+//				fmt.Print("_ ")
+//			}
+//		}
+//	}
+func Displayword(words string) string {
+	numbreveal := (len(words) / 2) - 1
+	tabword := make([]string, len(words))
+	for i := range tabword {
+		tabword[i] = "_"
+	}
+	for i := 0; i < numbreveal; i++ {
+		max := len(words) - 1
+		indexrandomword := rand.Intn(max)
+		randomletter := string(words[indexrandomword])
+		for j := 0; j < len(words); j++ {
+			if string(words[j]) == randomletter {
+				tabword[j] = randomletter
+			}
 		}
 	}
+	return strings.Join(tabword, "")
 }
